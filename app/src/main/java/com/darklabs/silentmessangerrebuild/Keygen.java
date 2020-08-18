f7f907a618c304a761c3749196dfdbdf778bc78c
D0kLabs <devtemp411@bigmir.net>
2020-08-18 15:20:52 +0300

    about sdk

---
+++ app/src/main/java/com/darklabs/silentmessangerrebuild/Keygen.java
@@ -0,0 +1,204 @@
+package com.darklabs.silentmessangerrebuild;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.util.Enumeration;
+
+import javax.crypto.SecretKey;
+
+public class Keygen {
+    KeyStore BluetoothKeys;
+    KeyStore OwnKeystore;
+    public static Certificate globalPublicCert = null;
+    public char[] passwd = ("1234567890").toCharArray();
+
+    public static boolean findByte (byte[] a, byte[] b){
+        boolean bool= false;
+        for (int i = 0; i <a.length ; i++) {
+            if (a[i]==b[0]){
+                for (int ii=1; ii<b.length; ii++){
+                    if (a[i+ii] != b[ii]) break;
+                    else if (ii == b.length-1) bool = true;
+                }
+            }
+        }
+        return bool;
+    }
+
+    {
+        try {
+            OwnKeystore = KeyStore.getInstance("AndroidCAStore");
+            BluetoothKeys = KeyStore.getInstance("AndroidCAStore");
+            InputStream io = new FileInputStream("OWNcertStore.store");
+            InputStream bt = new FileInputStream("BTcertStore.store");
+            OwnKeystore.load(io, passwd);
+            BluetoothKeys.load(bt, passwd);
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static KeyPairGenerator keyPairGen;
+
+    static {
+        try {
+            keyPairGen = KeyPairGenerator.getInstance("DSA");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static KeyPair two = keyPairGen.generateKeyPair();
+
+    public PrivateKey getgPrivateKey() {
+        PrivateKey gPrivateKey = two.getPrivate();
+        return gPrivateKey;
+    }
+
+    public static PublicKey getgPublicKey() throws CertificateException {
+        PublicKey gPublicKey = two.getPublic();
+        byte[] buffer = gPublicKey.getEncoded();
+
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(buffer);
+        CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+        while (byteArrayInputStream.available() > 0) {
+            globalPublicCert = certificateFactory.generateCertificate(byteArrayInputStream);
+            //PublicCerts.
+        }
+        return gPublicKey;
+    }
+
+
+    public boolean mBluetoothKeys(PublicKey mPublic) throws KeyStoreException {
+        boolean retBtKeyStore = false;
+        try {
+            BluetoothKeys.load(null);
+            Enumeration<String> trusted = BluetoothKeys.aliases();
+
+        } catch (CertificateException e) {
+            e.printStackTrace();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        }
+        if (keyStoreCompare(mPublic)) {
+            setBluetoothKeys(mPublic);
+            retBtKeyStore = true;
+        }
+        return retBtKeyStore;
+    }
+
+    private void setBluetoothKeys(Key mPublic) {
+        //FROM WIFI Public key to mPublic and its current
+        if (mPublic != null) {
+            javax.crypto.SecretKey currentPubKey = (SecretKey) mPublic;
+            KeyStore.SecretKeyEntry secretKeyEntry = new KeyStore.SecretKeyEntry(currentPubKey);
+            if (BluetoothTrs.trusted.peek() != null) {
+                String mName = BluetoothTrs.trusted.peek();
+                try {
+                    FileOutputStream fileOutputStream = new FileOutputStream(BluetoothTrs.trusted.poll(), false);
+                    fileOutputStream.write(currentPubKey.getEncoded());
+                    fileOutputStream.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            // TO ACTIVITY!
+            // else toast = R.string.error; ...
+        }
+    }
+
+    private void getBluetoothKeys() throws KeyStoreException {
+        String mfileName = fSearchFile(BluetoothTrs.trusted.peek());
+        FileInputStream fileInputStream = null;
+        try {
+            fileInputStream = new FileInputStream(mfileName);
+            fileInputStream.read();
+            javax.crypto.SecretKey currentKey = (SecretKey) fileInputStream;
+            if (keyStoreCompare(currentKey)) {
+                setBluetoothKeys(currentKey);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static String fSearchFile(String mName) {
+        File actual = new File(mName);
+        String fileName = "";
+        for (File z : actual.listFiles()) {
+            fileName = z.getName();
+        }
+        return fileName;
+    }
+
+    public boolean keyStoreCompare(Key Public) throws KeyStoreException {
+        boolean state = false;
+        String peek = BluetoothTrs.trusted.peek();
+        Enumeration<String> aliases = BluetoothKeys.aliases();
+        String mName = null;
+        if (BluetoothKeys.containsAlias(peek)) {
+            for (String i = null; i == peek; i = aliases.nextElement()) {
+                mName = i;
+            }
+            try {
+                while (mName != null) {
+                    if (BluetoothKeys.containsAlias(mName)) {
+                        if ((BluetoothKeys.getKey(mName, passwd)) == Public) {
+                            state = true;
+                        }
+                    }
+                }
+            } catch (KeyStoreException e) {
+                e.printStackTrace();
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            } catch (UnrecoverableKeyException e) {
+                e.printStackTrace();
+            }
+        }
+        return state;
+    }
+    public byte[] getEncrypted(byte[] data){
+        //Encrypt data
+        return data;
+    }
+    public byte[] getDecrypt(byte[] data){
+        //Decrypt data
+        return data;
+    }
+}
---
+++ app/src/main/java/com/darklabs/silentmessangerrebuild/MainActivity.java
@@ -0,0 +1,291 @@
+package com.darklabs.silentmessangerrebuild;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.wifi.WpsInfo;
+import android.net.wifi.p2p.WifiP2pConfig;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.nsd.WifiP2pServiceInfo;
+import android.net.wifi.p2p.nsd.WifiP2pServiceRequest;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.crypto.NoSuchPaddingException;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTextView;
+    private EditText mEditText;
+    private Button mSend;
+    private Box[] mBox;
+    private int index = 0;
+    public String[] BlackBox = new String[index];
+    private final IntentFilter mIntentFilter = new IntentFilter();
+    public WifiP2pManager.Channel mChannel;
+    public WifiP2pManager mWifiP2pManager;
+    public BroadcastReceiver mBroadcastReceiver;
+    public List peers = new ArrayList();
+    public WifiP2pManager.PeerListListener peerListListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextView = (TextView) findViewById(R.id.message_view);
+        mEditText = (EditText) findViewById(R.id.message_edit);
+        mSend = (Button) findViewById(R.id.Send);
+        BluetoothTrs.BtFinder();
+
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        mWifiP2pManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mWifiP2pManager.initialize(this, getMainLooper(), null);
+        peerListListener = new WifiP2pManager.PeerListListener() {
+
+            @Override
+            public void onPeersAvailable(WifiP2pDeviceList wifiP2pDeviceList) {
+                peers.clear();
+                peers.addAll(wifiP2pDeviceList.getDeviceList());
+
+            }
+        };
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mWifiP2pManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+                // TODO: IF discover init successful call peerFilter or add task to init request peers
+            }
+
+            @Override
+            public void onFailure(int i) {
+                // Toast user: "LOL! Toast" + i;
+            }
+        });
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            mWifiP2pManager.discoverServices(mChannel, new WifiP2pManager.ActionListener() {
+                String queryData = "0day_silent?";
+
+                //Certificate pubCert = Keygen.;
+                @Override
+                public void onSuccess() {
+                    mWifiP2pManager.addServiceRequest(mChannel, WifiP2pServiceRequest.newInstance(WifiP2pServiceInfo.SERVICE_TYPE_VENDOR_SPECIFIC, queryData), new WifiP2pManager.ActionListener() {
+                        @Override
+                        public void onSuccess() {
+                            mWifiP2pManager.setServiceResponseListener(mChannel, new WifiP2pManager.ServiceResponseListener() {
+                                @Override
+                                public void onServiceAvailable(int i, byte[] bytes, WifiP2pDevice wifiP2pDevice) {
+                                    byte q = ((byte) 255);
+                                    char[] chQeury = queryData.toCharArray();
+                                    byte[] bQeury = null;
+                                    bQeury = queryData.getBytes(StandardCharsets.UTF_8);
+                                    for (byte j = 0; j <= bytes.length; j++) {
+                                        if (bytes[j] == q) {
+                                            boolean anwer = (Keygen.findByte(bytes, bQeury));
+                                            // Huston we have our service!
+                                        } else {/* Tast> Huston: Warning! We have unsigned unknown service! */}
+
+                                    }
+                                }
+                            });
+
+                        }
+
+                        @Override
+                        public void onFailure(int i) {
+                            mWifiP2pManager.removeServiceRequest(mChannel, WifiP2pServiceRequest.newInstance(WifiP2pServiceInfo.SERVICE_TYPE_VENDOR_SPECIFIC), new WifiP2pManager.ActionListener() {
+                                @Override
+                                public void onSuccess() {
+                                    //reaper what is wrong
+                                }
+
+                                @Override
+                                public void onFailure(int i) {
+
+                                }
+                            });
+
+                        }
+                    });
+                }
+
+                @Override
+                public void onFailure(int i) {
+
+
+                }
+            });
+        }
+        mWifiP2pManager.requestPeers(mChannel, new WifiP2pManager.PeerListListener() {
+            @Override
+            public void onPeersAvailable(WifiP2pDeviceList wifiP2pDeviceList) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Sender();
+
+            }
+        });
+        mSend.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int keyCode, KeyEvent keyEvent) {
+                if (keyEvent.getAction() == KeyEvent.ACTION_DOWN && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    Sender();
+                }
+                return false;
+            }
+        });
+        for (int i = 0; i < index; i++) {
+            mTextView.setText(mBox[i].getTextResId());
+        }
+        mBroadcastReceiver = new mWiFiDirectBroadcastReceiver(mWifiP2pManager, mChannel, this);
+        registerReceiver(mBroadcastReceiver, mIntentFilter);
+    }
+
+    private boolean Sender() {
+        String msg = mEditText.getText().toString();
+
+        if (msg.isEmpty() == false) {
+            mBox[index].setTextResId(index);
+            try {
+                BlackBox[index] = mBox[index].setTextRes(msg);
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            } catch (NoSuchPaddingException e) {
+                e.printStackTrace();
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            mBox[index].setFilled(true);
+            mBox[index].setSize(msg.length());
+            index++;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(mBroadcastReceiver);
+
+    }
+
+    public class mWiFiDirectBroadcastReceiver extends BroadcastReceiver {
+
+        private Context mainActivity;
+
+        public mWiFiDirectBroadcastReceiver(WifiP2pManager mWifiP2pManager, WifiP2pManager.Channel channel, MainActivity mainActivity) {
+        }
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String resiver = intent.getAction();
+            if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(resiver)) {
+                int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+                if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+                    //Toast something)
+                }
+            } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(resiver)) {
+                if (ActivityCompat.checkSelfPermission(mainActivity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                mWifiP2pManager.requestPeers(mChannel, peerListListener);
+            } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(resiver)) {
+                //AHTUNG!!! Connection FAILS!
+            } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(resiver)) {
+                //TOAST "I zachem ti vikluchil wifi?"
+            }
+        }
+
+    }
+
+    public void connect() {
+        WifiP2pDevice device = (WifiP2pDevice) peers.get(0);
+        final WifiP2pConfig config = new WifiP2pConfig();
+        config.deviceAddress = device.deviceAddress;
+        config.wps.setup = WpsInfo.PBC;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mWifiP2pManager.connect(mChannel, config, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+                // It will work)
+            }
+
+            @Override
+            public void onFailure(int i) {
+            }
+        });
+
+    }
+
+
+
+
+    public byte[] WifiCleint(String SSIDName){
+        byte[] data = null;
+
+
+        return data;
+    }
+}
