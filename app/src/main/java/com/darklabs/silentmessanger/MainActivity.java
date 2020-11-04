cce1db4d9323438e3f952124bb70550a822b6d2c
D0kLabs <devtemp411@bigmir.net>
2020-11-04 14:08:28 +0200

    Wifi permissions

--- app/src/main/java/com/darklabs/silentmessanger/MainActivity.java
+++ app/src/main/java/com/darklabs/silentmessanger/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.net.NetworkInfo;
 import android.net.wifi.WpsInfo;
 import android.net.wifi.p2p.WifiP2pConfig;
 import android.net.wifi.p2p.WifiP2pDevice;
@@ -22,6 +23,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -58,22 +60,26 @@ public class MainActivity extends AppCompatActivity {
     public WifiP2pManager.Channel mChannel;
     public WifiP2pManager mWifiP2pManager;
     public BroadcastReceiver mBroadcastReceiver;
+    public WifiP2pManager.ConnectionInfoListener mInfoListener;
     public List peers = new ArrayList();
     public WifiP2pManager.PeerListListener peerListListener;
     String[] permissions = {ACCESS_FINE_LOCATION, ACCESS_NETWORK_STATE, BLUETOOTH, BLUETOOTH_ADMIN, ACCESS_WIFI_STATE, CHANGE_WIFI_STATE};
 
     // Function to check and request permission
-    public boolean checkPermission(String permission, int requestCode) {
+    public void checkerPermission(String permission, int requestCode) {
         // Checking if permission is not granted
         for (int i = 0; i < permissions.length; i++) {
             permission = permissions[i];
+            if (ContextCompat.checkSelfPermission(MainActivity.this, permission) == PackageManager.PERMISSION_DENIED) {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{permission}, requestCode);
+            }
         }
-        if (ContextCompat.checkSelfPermission(MainActivity.this, permission) == PackageManager.PERMISSION_DENIED) {
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{permission}, requestCode);
-        } else {
-            Toast.makeText(MainActivity.this, "Permission already granted", Toast.LENGTH_SHORT).show();
+    }
+    public void checkPermission (String permission, int requestCode)
+    {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, permission) == PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{permission},requestCode);
         }
-        return false;
     }
 
 
@@ -85,6 +91,7 @@ public class MainActivity extends AppCompatActivity {
         mEditText = findViewById(R.id.message_edit);
         mSend = findViewById(R.id.Send);
 
+
         mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
         mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
         mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
@@ -92,6 +99,7 @@ public class MainActivity extends AppCompatActivity {
 
         mWifiP2pManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
         mChannel = mWifiP2pManager.initialize(this, getMainLooper(), null);
+        mBroadcastReceiver = new mWiFiDirectBroadcastReceiver(mWifiP2pManager,mChannel, mInfoListener);
         peerListListener = new WifiP2pManager.PeerListListener() { //have one. Need some else?
 
             @Override
@@ -101,85 +109,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         };
-        if (checkPermission(ACCESS_FINE_LOCATION, ACCESS_FINE_LOCATION_CODE)) {
-            mWifiP2pManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
-                @Override
-                public void onSuccess() {
-                    // TODO: IF discover init successful, call peerFilter or add task to init request peers
-                }
-
-                @Override
-                public void onFailure(int i) {
-                    //wait 5 minutes
-                }
-            });
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                mWifiP2pManager.discoverServices(mChannel, new WifiP2pManager.ActionListener() {
-                    String queryData = "0day_silent?";
-
-                    @Override
-                    public void onSuccess() {
-                        mWifiP2pManager.addServiceRequest(mChannel, WifiP2pServiceRequest.newInstance(WifiP2pServiceInfo.SERVICE_TYPE_VENDOR_SPECIFIC, queryData), new WifiP2pManager.ActionListener() {
-                            @Override
-                            public void onSuccess() {
-                                mWifiP2pManager.setServiceResponseListener(mChannel, new WifiP2pManager.ServiceResponseListener() {
-                                    @Override
-                                    public void onServiceAvailable(int i, byte[] bytes, WifiP2pDevice wifiP2pDevice) { // what is "i" ?
-                                        byte[] q = new byte[]{((byte) 255)}; // ?wrong?
-                                        if (Keygen.findByte(bytes, q)) {
-                                            try {
-                                                Certificate localcert = BluetoothKeys.getCertificate(mEnumeration.nextElement());
-                                                byte[] bQeury = localcert.getEncoded();
-                                                boolean answer = (Keygen.findByte(bytes, bQeury));
-                                                if (answer) {
-                                                    //1 get cert alias
-                                                    //2 get compare hw info
-                                                }
-                                            } catch (KeyStoreException e) {
-                                                e.printStackTrace();
-                                            } catch (CertificateEncodingException e) {
-                                                e.printStackTrace();
-                                            }
-
-                                        } else {/* Tast> Huston: Warning! We have unsigned unknown service! */}
-
-                                    }
-                                });
-
-                            }
-
-                            @Override
-                            public void onFailure(int i) {
-                                mWifiP2pManager.removeServiceRequest(mChannel, WifiP2pServiceRequest.newInstance(WifiP2pServiceInfo.SERVICE_TYPE_VENDOR_SPECIFIC), new WifiP2pManager.ActionListener() {
-                                    @Override
-                                    public void onSuccess() {
-                                        // ...
-                                    }
-
-                                    @Override
-                                    public void onFailure(int i) {
-
-                                    }
-                                });
-
-                            }
-                        });
-                    }
-
-                    @Override
-                    public void onFailure(int i) {
-
-
-                    }
-                });
-            }
-            mWifiP2pManager.requestPeers(mChannel, new WifiP2pManager.PeerListListener() {
-                @Override
-                public void onPeersAvailable(WifiP2pDeviceList wifiP2pDeviceList) {
-
-                }
-            });
-        }
+        
     }
     @Override
     protected void onStart() {
@@ -205,7 +135,6 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
-        mBroadcastReceiver = new mWiFiDirectBroadcastReceiver(mWifiP2pManager, mChannel, this);
         registerReceiver(mBroadcastReceiver, mIntentFilter);
     }
 
@@ -227,41 +156,39 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public class mWiFiDirectBroadcastReceiver extends BroadcastReceiver {
+        public WifiP2pManager manager;
+        public WifiP2pManager.Channel channel;
+        public WifiP2pManager.ConnectionInfoListener mInfoListener;
 
-        private Context mainActivity;
-
-        public mWiFiDirectBroadcastReceiver(WifiP2pManager mWifiP2pManager, WifiP2pManager.Channel channel, MainActivity mainActivity) {
+        public mWiFiDirectBroadcastReceiver(WifiP2pManager manager, WifiP2pManager.Channel channel, WifiP2pManager.ConnectionInfoListener mInfoListener) {
+            super();
+            this.manager = manager;
+            this.channel = channel;
+            this.mInfoListener = mInfoListener;
         }
-
+        @RequiresApi(api = Build.VERSION_CODES.M)
         @Override
         public void onReceive(Context context, Intent intent) {
-            String resiver = intent.getAction();
-            if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(resiver)) {
-                int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
-                if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
-                    //Toast something)
-                }
-            } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(resiver)) {
-                if (ActivityCompat.checkSelfPermission(mainActivity, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return;
-                }
-                if (checkPermission(ACCESS_FINE_LOCATION, ACCESS_FINE_LOCATION_CODE)) { ///wtF?
-                    mWifiP2pManager.requestPeers(mChannel, peerListListener);
-                } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(resiver)) {
+            String action = intent.getAction();
+            if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)){
+                if (manager != null){
+                    final NetworkInfo networkInfo = intent.getParcelableExtra(WifiP2pManager.EXTRA_NETWORK_INFO);
+                if (networkInfo.isConnected()){
+                    manager.requestConnectionInfo(channel,mInfoListener);
+                } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+                checkPermission(ACCESS_FINE_LOCATION,ACCESS_FINE_LOCATION_CODE);
+                } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
                     //AHTUNG!!! Connection FAILS!
-                } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(resiver)) {
+                } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
                     //TOAST "I zachem ti vikluchil wifi?"
                 }
             }
         }
 
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+        }
     }
 
     public void connect() {
@@ -306,11 +233,93 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == ACCESS_FINE_LOCATION_CODE) {
+        switch (requestCode){
 
             // Checking whether user granted the permission or not.
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Toast.makeText(MainActivity.this, "FINE_LOCATION Permission Granted", Toast.LENGTH_SHORT).show();
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if ((checkPermission(ACCESS_FINE_LOCATION, ACCESS_FINE_LOCATION_CODE)) != PackageManager.PERMISSION_GRANTED){
+                        mWifiP2pManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
+                            @Override
+                            public void onSuccess() {
+                                mWifiP2pManager.requestPeers(channel, peerListListener);
+                            }
+
+                            @Override
+                            public void onFailure(int i) {
+                                //wait 5 minutes
+                            }
+                        });
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                        mWifiP2pManager.discoverServices(mChannel, new WifiP2pManager.ActionListener() {
+                            String queryData = "0day_silent?";
+
+                            @Override
+                            public void onSuccess() {
+                                mWifiP2pManager.addServiceRequest(mChannel, WifiP2pServiceRequest.newInstance(WifiP2pServiceInfo.SERVICE_TYPE_VENDOR_SPECIFIC, queryData), new WifiP2pManager.ActionListener() {
+                                    @Override
+                                    public void onSuccess() {
+                                        mWifiP2pManager.setServiceResponseListener(mChannel, new WifiP2pManager.ServiceResponseListener() {
+                                            @Override
+                                            public void onServiceAvailable(int i, byte[] bytes, WifiP2pDevice wifiP2pDevice) { // what is "i" ?
+                                                byte[] q = new byte[]{((byte) 255)}; // ?wrong?
+                                                if (Keygen.findByte(bytes, q)) {
+                                                    try {
+                                                        Certificate localcert = BluetoothKeys.getCertificate(mEnumeration.nextElement());
+                                                        byte[] bQeury = localcert.getEncoded();
+                                                        boolean answer = (Keygen.findByte(bytes, bQeury));
+                                                        if (answer) {
+                                                            //1 get cert alias
+                                                            //2 get compare hw info
+                                                        }
+                                                    } catch (KeyStoreException e) {
+                                                        e.printStackTrace();
+                                                    } catch (CertificateEncodingException e) {
+                                                        e.printStackTrace();
+                                                    }
+
+                                                } else {/* Tast> Huston: Warning! We have unsigned unknown service! */}
+
+                                            }
+                                        });
+
+                                    }
+
+                                    @Override
+                                    public void onFailure(int i) {
+                                        mWifiP2pManager.removeServiceRequest(mChannel, WifiP2pServiceRequest.newInstance(WifiP2pServiceInfo.SERVICE_TYPE_VENDOR_SPECIFIC), new WifiP2pManager.ActionListener() {
+                                            @Override
+                                            public void onSuccess() {
+                                                // ...
+                                            }
+
+                                            @Override
+                                            public void onFailure(int i) {
+
+                                            }
+                                        });
+
+                                    }
+                                });
+                            }
+
+                            @Override
+                            public void onFailure(int i) {
+
+
+                            }
+                        });
+                    }
+                    mWifiP2pManager.requestPeers(mChannel, new WifiP2pManager.PeerListListener() {
+                        @Override
+                        public void onPeersAvailable(WifiP2pDeviceList wifiP2pDeviceList) {
+
+                        }
+                    });
+                }
+            }
+            
             } else {
                 Toast.makeText(MainActivity.this, "FINE_LOCATION Permission Denied", Toast.LENGTH_SHORT).show();
             }
